e79759c52888fa55a9e08ac89363f736cb2d112b
김준기 <wnsrl981228@pinnode.com>
2024-12-06 00:18:37 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,145 @@
+from flask import Flask, request, jsonify, redirect, send_file
+import os
+from flask_cors import CORS
+import base64
+from PIL import Image
+import logging
+import sys
+
+# static 폴더 경로를 sys.path에 추가
+sys.path.append(os.path.join(os.path.dirname(__file__), 'static'))
+# 로깅 설정
+logging.basicConfig(level=logging.DEBUG)  # 디버그 레벨 로그 출력
+
+# 이제 static/images-manifest.py에서 ImagesManifest 클래스 사용 가능
+from imagesManifest import ImagesManifest
+
+
+
+app = Flask(__name__)
+CORS(app)  # CORS 설정 추가
+
+# manifest 요청
+@app.route('/api/iiif/v2/kmManifests/<string:identifier>/manifest.json', methods=['GET'])
+def request_km_manifest(identifier):
+    
+    ###############################################
+    #  json_data에 manifest.json 값 넣으면 됩니다. #
+    ###############################################
+    json_data = ''
+    if identifier == '2078933':
+        json_data = ImagesManifest.MANIFEST
+    response = jsonify(json_data)
+    response.headers.set("Content-Type", "application/json;charset=UTF-8")
+
+    return response
+
+# 이미지 요청
+@app.route('/api/iiif/v2/kmImages/<string:identifier>/<string:region>/<string:size>/<string:rotation>/<string:quality>.<string:format>', methods=['GET'])
+def iiif_view_km_image(identifier, region, size, rotation, quality, format):
+
+    f = safe_decode(identifier)
+    arr_file = f.split("::")
+
+    down_index, down_folder, down_file_name, org_file_name = arr_file
+    file_path = os.path.join("./", down_folder, down_file_name)
+
+    # 파일 존재 여부 확인
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    # 파일 확장자 추출
+    format_code = os.path.splitext(file_path)[1].lstrip('.')
+
+    # Content-Type 설정
+    mime_type = "image/"+format_code
+    print("===")
+    print(down_file_name)
+    print(down_folder)
+    # 이미지 파일을 반환
+    return send_file(file_path, mimetype=mime_type)
+
+
+# 첫 번째 요청 - Redirect 처리 : 이미지 정보 요청
+@app.route('/api/iiif/v2/kmImages/<string:identifier>', methods=['GET'])
+def redirect_image_info(identifier):
+    domain = request.host_url.rstrip('/')
+    redirect_url = f"{domain}{request.path}/info.json"
+    return redirect(redirect_url, code=303)  # HTTP 303 See Other
+
+# 두 번째 요청 - Image Info 처리 : 이미지 정보 요청
+@app.route('/api/iiif/v2/kmImages/<string:identifier>/info.json', methods=['GET'])
+def iiif_view_km_image_info(identifier):
+    # 실제 처리 로직 추가 필요
+    
+    f = safe_decode(identifier)
+    arr_file = f.split("::")
+
+    down_index, down_folder, down_file_name, org_file_name = arr_file
+    file_path = os.path.join("./", down_folder, down_file_name)
+
+    # 파일 존재 여부 확인
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File not found: {file_path}")
+
+    # 파일 확장자 추출
+    format_code = os.path.splitext(file_path)[1].lstrip('.')
+    
+    # 이미지 너비와 높이 추출
+    try:
+        with Image.open(file_path) as img:
+            width, height = img.size
+    except Exception as e:
+        raise ValueError(f"Failed to get image dimensions: {e}")
+
+
+
+    data = {
+        "@context": "http://iiif.io/api/image/2/context.json",
+        "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDExMjdteFNRLnBuZzo67Iqk7YGs66aw7IO3IDIwMjMtMDMtMDYgMDAyOTA0LnBuZw==",
+        "@type": "iiif:Image",
+        "protocol": "http://iiif.io/api/image",
+        "profile": [
+            "http://iiif.io/api/image/2/level0.json",
+            {
+            "formats": [
+                format_code
+            ],
+            "qualities": [
+                "default"
+            ],
+            "supports": [
+                "sizeByWhListed",
+                "baseUriRedirect",
+                "cors",
+                "jsonldMediaType"
+            ]
+            }
+        ],
+        "sizes": [
+            {
+            "width": width,
+            "height": height
+            }
+        ],
+        "width": width,
+        "height": height
+    }
+    return jsonify(data)  # JSON 형태로 응답
+
+
+
+
+def safe_decode(encoded_str):
+    """
+    URL-safe Base64로 인코딩된 문자열을 디코딩합니다.
+    :param encoded_str: URL-safe Base64로 인코딩된 문자열
+    :return: 디코딩된 문자열
+    """
+    decoded_bytes = base64.urlsafe_b64decode(encoded_str)
+    return decoded_bytes.decode('utf-8')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
+    # app.run(debug=True,  port=5000)
---
+++ static/imagesManifest.py
@@ -0,0 +1,166 @@
+class ImagesManifest:
+    MANIFEST = {
+    "@context": "http://iiif.io/api/presentation/2/context.json",
+    "@id": "http://localhost:8080/api/iiif/v2/kmManifests/2078933/manifest.json",
+    "@type": "sc:Manifest",
+    "label": "이현세 초기 작품이 보여주는 혁신성 제목",
+    "metadata": [
+        {
+        "label": "제목",
+        "value": "이현세 초기 작품이 보여주는 혁신성 제목"
+        },
+        {
+        "label": "발행자",
+        "value": "발행자입니다"
+        },
+        {
+        "label": "설명",
+        "value": "이현세의 길 : K-웹툰 전설의 시작 특별전 학술행사\r\n\r\n이현세 학술행사 발제 1_박인하(만화평론가)\r\n\r\n\r\n* 전체 다운로드: https://www.oak.go.kr/nl-ir/handle/2020.oak/1058"
+        },
+        {
+        "label": "발행일자",
+        "value": "20240529"
+        }
+    ],
+    "sequences": [
+        {
+        "@id": "http://localhost:8080/api/iiif/v2/manifests/2078933/sequence/s0",
+        "@type": "sc:Sequence",
+        "viewingHint": "paged",
+        "canvases": [
+            {
+            "@id": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p1",
+            "@type": "sc:Canvas",
+            "label": "1",
+            "thumbnail": {
+                "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDA2MjBQMnlwLnBuZzo67J207ZiE7IS4IOy0iOq4sCDsnpHtkojsnbQg67O07Jes7KO864qUIO2YgeyLoOyEsS5wbmc=/full/max/0/default.jpg",
+                "@type": "dctypes:Image",
+                "height": 149,
+                "width": 167
+            },
+            "height": 149,
+            "width": 167,
+            "images": [
+                {
+                "@id": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p1/painting/anno",
+                "@type": "oa:Annotation",
+                "motivation": "sc:painting",
+                "resource": {
+                    "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDA2MjBQMnlwLnBuZzo67J207ZiE7IS4IOy0iOq4sCDsnpHtkojsnbQg67O07Jes7KO864qUIO2YgeyLoOyEsS5wbmc=/full/max/0/default.jpg",
+                    "@type": "dctypes:Image",
+                    "format": "image/png",
+                    "service": {
+                    "@context": "http://iiif.io/api/image/2/context.json",
+                    "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDA2MjBQMnlwLnBuZzo67J207ZiE7IS4IOy0iOq4sCDsnpHtkojsnbQg67O07Jes7KO864qUIO2YgeyLoOyEsS5wbmc=",
+                    "profile": "http://iiif.io/api/image/2/level0.json"
+                    },
+                    "height": 149,
+                    "width": 167
+                },
+                "on": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p1"
+                }
+            ]
+            },
+            {
+            "@id": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p2",
+            "@type": "sc:Canvas",
+            "label": "2",
+            "thumbnail": {
+                "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDExMjd0NVkwLnBuZzo67Iqk7YGs66aw7IO3IDIwMjMtMDUtMjEgMDAyODI4LnBuZw==/full/max/0/default.jpg",
+                "@type": "dctypes:Image",
+                "height": 4032,
+                "width": 3024
+            },
+            "height": 4032,
+            "width": 3024,
+            "images": [
+                {
+                "@id": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p2/painting/anno",
+                "@type": "oa:Annotation",
+                "motivation": "sc:painting",
+                "resource": {
+                    "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDExMjd0NVkwLnBuZzo67Iqk7YGs66aw7IO3IDIwMjMtMDUtMjEgMDAyODI4LnBuZw==/full/max/0/default.jpg",
+                    "@type": "dctypes:Image",
+                    "format": "image/png",
+                    "service": {
+                    "@context": "http://iiif.io/api/image/2/context.json",
+                    "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDExMjd0NVkwLnBuZzo67Iqk7YGs66aw7IO3IDIwMjMtMDUtMjEgMDAyODI4LnBuZw==",
+                    "profile": "http://iiif.io/api/image/2/level0.json"
+                    },
+                    "height": 4032,
+                    "width": 3024
+                },
+                "on": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p2"
+                }
+            ]
+            },
+            {
+            "@id": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p3",
+            "@type": "sc:Canvas",
+            "label": "3",
+            "thumbnail": {
+                "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDExMjc4TTVGLnBuZzo67Iqk7YGs66aw7IO3IDIwMjMtMDQtMTUgMDAwMjM3LnBuZw==/full/max/0/default.jpg",
+                "@type": "dctypes:Image",
+                "height": 449,
+                "width": 473
+            },
+            "height": 449,
+            "width": 473,
+            "images": [
+                {
+                "@id": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p3/painting/anno",
+                "@type": "oa:Annotation",
+                "motivation": "sc:painting",
+                "resource": {
+                    "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDExMjc4TTVGLnBuZzo67Iqk7YGs66aw7IO3IDIwMjMtMDQtMTUgMDAwMjM3LnBuZw==/full/max/0/default.jpg",
+                    "@type": "dctypes:Image",
+                    "format": "image/png",
+                    "service": {
+                    "@context": "http://iiif.io/api/image/2/context.json",
+                    "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDExMjc4TTVGLnBuZzo67Iqk7YGs66aw7IO3IDIwMjMtMDQtMTUgMDAwMjM3LnBuZw==",
+                    "profile": "http://iiif.io/api/image/2/level0.json"
+                    },
+                    "height": 449,
+                    "width": 473
+                },
+                "on": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p3"
+                }
+            ]
+            },
+            {
+            "@id": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p4",
+            "@type": "sc:Canvas",
+            "label": "4",
+            "thumbnail": {
+                "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDExMjdteFNRLnBuZzo67Iqk7YGs66aw7IO3IDIwMjMtMDMtMDYgMDAyOTA0LnBuZw==/full/max/0/default.jpg",
+                "@type": "dctypes:Image",
+                "height": 51,
+                "width": 73
+            },
+            "height": 51,
+            "width": 73,
+            "images": [
+                {
+                "@id": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p4/painting/anno",
+                "@type": "oa:Annotation",
+                "motivation": "sc:painting",
+                "resource": {
+                    "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDExMjdteFNRLnBuZzo67Iqk7YGs66aw7IO3IDIwMjMtMDMtMDYgMDAyOTA0LnBuZw==/full/max/0/default.jpg",
+                    "@type": "dctypes:Image",
+                    "format": "image/png",
+                    "service": {
+                    "@context": "http://iiif.io/api/image/2/context.json",
+                    "@id": "http://localhost:8080/api/iiif/v2/kmImages/OjppdGVtOjpmMjAyNDExMjdteFNRLnBuZzo67Iqk7YGs66aw7IO3IDIwMjMtMDMtMDYgMDAyOTA0LnBuZw==",
+                    "profile": "http://iiif.io/api/image/2/level0.json"
+                    },
+                    "height": 51,
+                    "width": 73
+                },
+                "on": "http://localhost:8080/api/iiif/v2/manifests/2078933/canvas/p4"
+                }
+            ]
+            }
+        ]
+        }
+    ]
+    }
